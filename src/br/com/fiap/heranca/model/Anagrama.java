8425439e4b0766faa07c4bb8c1c7ea42793da860
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-28 22:05:25 -0300

    Anagrama e Palindromo

---
+++ src/br/com/fiap/heranca/model/Anagrama.java
@@ -0,0 +1,18 @@
+package br.com.fiap.heranca.model;
+
+public class Anagrama {
+
+	public static void main(String[] args) {
+		//Tem a mesma quantidade de letras, porém troca a ordem das letras.
+		
+		String p1 = "casa";
+		String p2 = "saca";
+		String p3 = "aacs";
+		
+		String a1[] = {"c", "a", "s", "a"};
+		String a1[] = {"s", "a", "c", "a"};
+		//String a2naoanagrama[] = {"c", "a", "c", "a"};
+
+	}
+
+}
---
+++ src/br/com/fiap/heranca/model/Palindromo.java
@@ -0,0 +1,13 @@
+package br.com.fiap.heranca.model;
+
+public class Palindromo {
+
+	public static void main(String[] args) {
+		
+		// natan
+		String palavra[] = {"n", "a", "t", "a", "n"};
+		//String palavra[] = {"f", "l", "a", "v", "i", "o"};
+
+	}
+
+}
