5d78c19d767f2b4f90bfd08a05d2dd453f614f17
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-28 22:07:30 -0300

    Palindromo

--- src/br/com/fiap/heranca/model/Palindromo.java
+++ src/br/com/fiap/heranca/model/Palindromo.java
@@ -4,6 +4,8 @@ public class Palindromo {
 
 	public static void main(String[] args) {
 		
+		//Palavra que tem o mesmo significado de trás para frente.
+		
 		// natan
 		String palavra[] = {"n", "a", "t", "a", "n"};
 		//String palavra[] = {"f", "l", "a", "v", "i", "o"};
